6b0859ef83ebd652e74042e34741a673f7118386
GavinThomas1192 <gthomas1192@gmail.com>
2017-10-18 08:54:27 -0700

    not wokring live

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -13,7 +13,7 @@ import Hero from '../hero-container';
 class App extends React.Component {
 
 
-  componentDidMount() {
+  componentWillMount() {
     let token = utils.cookieFetch('X-Sluggram-Token');
     if(token) this.props.tokenSet(token);
   }
