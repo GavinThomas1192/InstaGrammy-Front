bc45b97afdcc729b895d977f3bf0456892e5c3c7
GavinThomas1192 <gthomas1192@gmail.com>
2017-10-18 10:30:23 -0700

    readfile

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const fs = require('fs');
+
 
 
 const app =  express();
 
 app.use(express.static(`${__dirname}/build`));
-app.get('*', (req, res) => fs.readFile(`${__dirname}/build/index.html`));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
 app.listen(process.env.PORT, () => console.log(`listening on port: ${process.env.PORT}`));
