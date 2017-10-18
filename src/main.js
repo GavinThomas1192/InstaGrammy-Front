e76683e3b6fb85f6bf756ba0a5cf3eb7aa4d3e96
GavinThomas1192 <gthomas1192@gmail.com>
2017-10-18 10:57:18 -0700

    readfile

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import ReactDom from 'react-dom';
 import App from './component/app';
 import {Provider} from 'react-redux';
 import appCreateStore from './lib/app-create-store';
+import * as utils from '../../lib/utils';
 
 let store = appCreateStore();
 
 class AppContainer extends React.Component {
+  componentDidMount() {
+    let token = utils.cookieFetch('X-Sluggram-Token');
+    if(token) this.props.tokenSet(token);
+  }
   render() {
     return (
       <Provider store={store}>
